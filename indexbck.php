19f4d4e8318e947479cc52c61df1e4256518a2ea
sarsarfi <sarasarfi79@gmail.com>
2025-06-07 12:54:40 +0330

    Add files via upload

---
+++ indexbck.php
@@ -0,0 +1,13 @@
+<?php
+
+define('ROOT_PATH', __DIR__);
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Autoload Composer و فایل‌های اصلی پروژه
+require_once ROOT_PATH . '/vendor/autoload.php';
+
+echo "<h1>Welcome to the Initial Project Setup!</h1>";
+echo "<p>Core files are loaded. Routing and other functionalities will be added in subsequent steps.</p>";
